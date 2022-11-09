a4a05fe20300f843badc05b409d70beab97d4fde
gabzcah <107358880+Rivaly-Kun@users.noreply.github.com>
2022-11-10 03:14:27 +0800

    Add files via upload

---
+++ response.js
@@ -0,0 +1,10 @@
+const responseObj = {
+  hello: "Hey! How you doing?",
+  hey: "Hey! What's Up",
+  potangina: "pakyu",
+  what_can_I_trade_it_with: "I want a tv set",
+  deal:"Noice",
+  today: new Date().toDateString(),
+  what_time: "how about 7am tommorow",
+  g:"gege"
+};
